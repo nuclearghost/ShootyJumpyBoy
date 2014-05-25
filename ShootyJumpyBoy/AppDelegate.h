2f84df34425bad7621b9bc7a55e5a799b291e78d
Mark Meyer <mark.meyer37@gmail.com>
2014-05-25 15:49:31 -0500

    Initial Commit

---
+++ ShootyJumpyBoy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShootyJumpyBoy
+//
+//  Created by Mark Meyer on 5/25/14.
+//  Copyright (c) 2014 Mark Meyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShootyJumpyBoy/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ShootyJumpyBoy
+//
+
+//  Copyright (c) 2014 Mark Meyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
