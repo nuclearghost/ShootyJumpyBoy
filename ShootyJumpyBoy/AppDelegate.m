e487a39500909fbd92f8715b7a38ed23f5f602b0
Mark Meyer <mark.meyer37@gmail.com>
2014-06-10 22:42:05 -0500

    Added settings and background transitions

--- ShootyJumpyBoy/AppDelegate.m
+++ ShootyJumpyBoy/AppDelegate.m
@@ -35,6 +35,8 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    SKView *view = (SKView *)self.window.rootViewController.view;
+    view.paused = YES;
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
@@ -46,6 +48,8 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    SKView *view = (SKView *)self.window.rootViewController.view;
+    view.paused = NO;
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
